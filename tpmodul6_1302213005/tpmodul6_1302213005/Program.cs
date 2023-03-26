faa150fae50c8dc84f17fa47db36588e44e577dc
andryfall <andryfall@student.telkomuniversity.ac.id>
2023-03-26 19:37:09 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_1302213005/tpmodul6_1302213005/Program.cs
+++ tpmodul6_1302213005/tpmodul6_1302213005/Program.cs
@@ -1,2 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+class program
+{
+    static void Main(string[] args)
+    {
+        SayaTubeVideo t = new SayaTubeVideo("Tutorial Design By Contract - Andry Nur Falah");
+        t.IncreasePlayCount(60);
+        t.PrintVideoDetails();
+    }
+}
+
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        Random rand = new Random();
+        this.title = title;
+        this.id = rand.Next();
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int playCount)
+    {
+        this.playCount += playCount;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("Title Video : " +  this.title);
+        Console.WriteLine("Id Video : "+ this.id);
+        Console.WriteLine("Durasi : " + this.playCount);
+    }
+}
